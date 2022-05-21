168b68d539e81a252431be36bd909dbf78f85b75
Valeria Aguirre <102261096+aguirre-valeria@users.noreply.github.com>
2022-05-21 19:49:00 -0300

    Tarea de la Clase 6: Creación un programa para adivinar un numero

---
+++ TareaClase6/TareaClase6/Program.cs
@@ -0,0 +1,32 @@
+﻿int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+
+Console.WriteLine("Adivina el número que elegió la computadora");
+int intentos = 0;
+
+while (true)
+{
+    Console.WriteLine("Ingresa un número");
+    int respuesta = 0;
+    respuesta = int.Parse(Console.ReadLine());
+
+    if (respuesta == numeroSecreto)
+    {
+        intentos++;
+        Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: {numeroSecreto}");
+        Console.WriteLine($"Lo has logrado en {intentos} intentos");
+        break;
+    }
+    else if (respuesta > numeroSecreto)
+    {
+        Console.WriteLine("El número ingresado es mayor al número secreto");
+        Console.WriteLine("Intenta nuevamente");
+        intentos++;
+    }
+    else if (respuesta < numeroSecreto)
+    {
+        Console.WriteLine("El número ingresado es menor al número secreto");
+        Console.WriteLine("Intenta nuevamente");
+        intentos++;
+    }
+}
